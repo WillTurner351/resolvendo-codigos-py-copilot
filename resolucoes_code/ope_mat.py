73324564d28f6d86a399a30bbf67704076d0627b
WillTurner <willtunner351@gmail.com>
2024-10-14 17:03:45 -0300

    Desafio resolucoes_code  em Python

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,17 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+operaçao = input("Digite a operação que deseja realizar (+,-,*,/:)")
+
+if operaçao == '+':
+    print(num1 + num2)
+elif operaçao == '-':
+    print(abs(num1 - num2))    
+if operaçao == '*':
+    print(num1 * num2)
+elif operaçao == '/':
+    print(num1 / num2)
+else:
+    print("Operação inválida")
--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,6 @@
-# Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+# Vamos solicitar como entrada uma string e um número e depois vamos realizar uma operação simples entre eles.
+
+string = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+print((string + '') * numero)
